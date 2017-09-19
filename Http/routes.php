10bcad363f2dab412b31c990f34ae99a8e1e825d
diuliano <diuliano0@gmail.com>
2017-09-19 10:47:56 -0300

    fix

--- Http/routes.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-Route::group(['middleware' => 'web', 'prefix' => 'anuncio', 'namespace' => 'Modules\Anuncio\Http\Controllers'], function()
-{
-    Route::get('/', 'AnuncioController@index');
-});
